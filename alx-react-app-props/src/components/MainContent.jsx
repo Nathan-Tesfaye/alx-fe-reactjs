927e5c3483c287ed0a78da274d5739661b47f3d1
Nathan Tesfaye <nathantesfaye434343@gmail.com>
2024-11-10 16:10:22 +0300

    props added

---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+function MainContent() {
+  return (
+    <main>
+      <p style={{
+        backgroundColor: 'limegreen',
+        fontWeight: 'bold',
+        color: 'red'
+      }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
