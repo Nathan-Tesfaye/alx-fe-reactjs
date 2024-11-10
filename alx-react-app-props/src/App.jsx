8b75e79c00e0ad323db628e248897bbc7fa5bf06
Nathan Tesfaye <nathantesfaye434343@gmail.com>
2024-11-10 17:33:11 +0300

    useContext

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,24 +1,16 @@
-import UserProfile from './components/UserProfile';
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
+import ProfilePage from './components/ProfilePage';
+import { UserContext } from './components/UserContext';
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
-      <div>
-          <WelcomeMessage />
-          <Header />
-          <MainContent />
-          <Footer />
-          <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      </div>
-  );
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  )
 }
 
-
-export default App
+export default App;
---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import UserDetails from './UserDetails';
+import { UserContext } from './UserContext';
+
+function ProfilePage() {
+  return <UserDetails />;
+}
+
+export default ProfilePage;
