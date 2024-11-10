1d0bbd838ddb7eb6f898f1865c310400a93e0dd9
Nathan Tesfaye <nathantesfaye434343@gmail.com>
2024-11-10 19:30:59 +0300

    components edit

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Pages/Home';
-import About from './components/Pages/About';
-import Services from './components/Pages/Services';
-import Contact from './components/Pages/Contact';
+import Home from './components/Home';
+import About from './components/About'
+import Services from './components/Services';
+import Contact from './components/Contact';
 import Navbar from './components/Navbar';
  
 
