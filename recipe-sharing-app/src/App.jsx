16b11362ec554ba105c454b8721f5d80d23750c5
Nathan Tesfaye <nathantesfaye434343@gmail.com>
2024-11-19 20:42:32 +0300

    task 0 done again

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
-import { RecipeList } from "./componenets/RecipeList";
-import { AddRecipeForm } from "./componenets/AddRecipeForm";
+import { RecipeList } from "./components/RecipeList";
+import { AddRecipeForm } from "./components/AddRecipeForm";
 
 function App() {
   
