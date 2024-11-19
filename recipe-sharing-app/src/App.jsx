ab54661b49ee0c884f71f1bbbf2707392c15264f
Nathan Tesfaye <nathantesfaye434343@gmail.com>
2024-11-19 20:39:03 +0300

    task 0 done

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { RecipeList } from "./componenets/RecipeList";
+import { AddRecipeForm } from "./componenets/AddRecipeForm";
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <RecipeList />
+        <AddRecipeForm />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ recipe-sharing-app/src/componenets/recipeStore.js
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+
+export const useRecipeStore = create( set => ({
+  recipes: [
+    {
+      id: 1, title: 'chocolate cake', description: 'baking'
+    },
+    {
+      id: 2, title: 'ice-cream', description: 'melt'
+    }
+  ],
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes })
+}));
+
+
+
